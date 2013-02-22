0d23c92c4c543bac0109bb6c5744117ff7208eb0
Michael Reddick <michael.reddick@gmail.com>
2013-02-21 22:44:25 -0600

    Card class

--- src/main/scala/card.scala
+++ src/main/scala/card.scala
@@ -1,6 +1,14 @@
 package poker
 
-class Card (
-  val rank :Int,
-  val suit :Char ) {
+class Card ( val r :Char, val s :Char ) {
+  val rank = r.toUpper
+  val suit = s.toLower
+
+  require(rank.toString.matches("[23456789TJQKA]"))
+  require(suit.toString.matches("[dhsc]"))
+
+  def this(st :String) = this(st.charAt(0), st.charAt(1));
+
+  override def toString = rank.toString + suit.toString
+
 }
--- src/test/scala/card.scala
+++ src/test/scala/card.scala
@@ -2,8 +2,39 @@ import org.scalatest.FunSuite
 import poker._
 
 class CardSuite extends FunSuite {
-  test("create a card") {
-    val card = new Card( 3, 'H');
-    assert(card.rank === 3);
+  test("New card") {
+    val card = new Card( '2', 'h')
+    assert(card.rank === '2')
   }
+
+  test("Invalid rank") {
+    try {
+      val card = new Card( '1', 'h')
+      fail()
+    }
+    catch {
+      case _: IllegalArgumentException =>
+    }
+  }
+
+  test("Invalid suit") {
+    try {
+      val card = new Card( '2', 'a')
+      fail()
+    }
+    catch {
+      case _: IllegalArgumentException =>
+    }
+  }
+
+  test("toString") {
+    assert(new Card( 'a', 's').toString === "As")
+    assert(new Card( 'k', 'h').toString === "Kh")
+    assert(new Card( '3', 'D').toString === "3d")
+  }
+
+  test("new Card(String)") {
+    assert(new Card("Ah").toString === "Ah");
+  }
+
 }
