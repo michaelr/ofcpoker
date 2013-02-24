1597b3bb2aa811af3a407fe70ccc7b1b7dbce678
Michael Reddick <michael.reddick@gmail.com>
2013-02-24 10:01:00 -0600

    Make deck init less verbose

--- src/main/scala/org/ofcpoker/Deck.scala
+++ src/main/scala/org/ofcpoker/Deck.scala
@@ -2,9 +2,7 @@ package org.ofcpoker
 
 class Deck {
   val deck =
-    List("A","K","Q","J","T","9","8","7","6","5","4","3","2")
-      .map(rank => List("s","c","h","d")
-        .map(suit => new Card(rank + suit)))
-      .flatten.toArray
-
+    "AKQJT98765432".split(".").map(rank =>
+      "schd".split(".").map(suit => new Card(rank + suit)))
+    .flatten.toArray
 }
