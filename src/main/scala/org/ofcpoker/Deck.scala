e78f12c388b5b7434d33ec26022898d5ca1b4485
Michael Reddick <michael.reddick@gmail.com>
2013-03-03 19:22:09 -0600

    Implement deal card

--- src/main/scala/org/ofcpoker/Deck.scala
+++ src/main/scala/org/ofcpoker/Deck.scala
@@ -1,7 +1,17 @@
 package org.ofcpoker
 
 class Deck {
-  val deck =
+  var deck =
     "AKQJT98765432".flatMap(rank => "schd".map(suit => new Card(rank.toString + suit)))
     .toArray
+
+  def cardsLeft = deck.size
+  def isEmpty   = cardsLeft == 0
+
+  def deal :Card = {
+    val random = scala.util.Random.nextInt(cardsLeft - 1)
+    val nextCard = deck(random)
+    deck = (deck take random) ++ (deck drop (random + 1))
+    return nextCard;
+  }
 }
--- src/test/scala/org/ofcpoker/deck.scala
+++ src/test/scala/org/ofcpoker/deck.scala
@@ -4,5 +4,12 @@ import org.ofcpoker._
 class DeckSuite extends FunSuite {
   test("New Deck") {
     val deck = new Deck
+    assert(deck.cardsLeft == 52)
+  }
+
+  test("deal") {
+    val deck = new Deck
+    assert(deck.deal.isInstanceOf[Card])
+    assert(deck.cardsLeft == 51)
   }
 }
